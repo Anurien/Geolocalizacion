96b52aa18df3ad7f27c13b2ed7555704cba71fec
Nuria <ngarciacobas@danielcastelao.org>
2023-02-23 13:57:29 +0100

    Añadido metodo que genera marcas aleatorias

--- app/src/main/java/com/example/geolocalizacion/MapsActivity.kt
+++ app/src/main/java/com/example/geolocalizacion/MapsActivity.kt
@@ -15,6 +15,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
@@ -22,6 +24,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    val latitude = 42.23639016660114
+    val longitude = -8.71412387331969
+    val latLng: LatLng = LatLng(latitude, longitude);
+
 
     companion object {
         const val REQUEST_LOCATION = 0
@@ -58,16 +64,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         //Cuando se  ha cargado el mapa le decimos que activa la localización
         enableLocation()
+        mMap.addMarker(MarkerOptions().position(getRandomLocation(latLng, 100)))
     }
 
     private fun createMarker() {
         val vigo = LatLng(42.23282, -8.72264)
         mMap.addMarker(MarkerOptions().position(vigo).title("Vigo"))
         mMap.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(vigo, 18f),
+            CameraUpdateFactory.newLatLngZoom(vigo, 15f),
             4000,
             null
         )
@@ -179,5 +186,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
      */
     override fun onMyLocationClick(p0: Location) {
         Toast.makeText(this, "Estás en ${p0.latitude},${p0.longitude} ", Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     *Método en el que introduces unas coodernadas y un radio y genera marcas aleatorias en el mapa
+     * @param point : el punto de donde se van a generar las marcas
+     * @param radius : el radio en metros del maximo donde aparecen las marcas
+     */
+    private fun getRandomLocation(point: LatLng, radius: Int): LatLng {
+        val randomPoints: MutableList<LatLng> = ArrayList()
+        val randomDistances: MutableList<Float> = ArrayList()
+        val myLocation = Location("latLng")
+        myLocation.latitude = point.latitude
+        myLocation.longitude = point.longitude
+
+        //This is to generate 10 random points
+        for (i in 0..9) {
+            val x0 = point.latitude
+            val y0 = point.longitude
+            val random = Random()
+
+            // Convert radius from meters to degrees
+            val radiusInDegrees = (radius / 111000f).toDouble()
+            val u: Double = random.nextDouble()
+            val v: Double = random.nextDouble()
+            val w = radiusInDegrees * Math.sqrt(u)
+            val t = 2 * Math.PI * v
+            val x = w * Math.cos(t)
+            val y = w * Math.sin(t)
+
+            // Adjust the x-coordinate for the shrinking of the east-west distances
+            val new_x = x / Math.cos(y0)
+            val foundLatitude = new_x + x0
+            val foundLongitude = y + y0
+            val randomLatLng = LatLng(foundLatitude, foundLongitude)
+            randomPoints.add(randomLatLng)
+            val l1 = Location("latLng")
+            l1.latitude = randomLatLng.latitude
+            l1.longitude = randomLatLng.longitude
+            randomDistances.add(l1.distanceTo(myLocation))
+        }
+        //Get nearest point to the centre
+        val indexOfNearestPointToCentre = randomDistances.indexOf(Collections.min(randomDistances))
+        return randomPoints[indexOfNearestPointToCentre]
     }
 }
